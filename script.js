a460a775f270bce78526a8a0f368cfcaf1dcafeb
lavraicevic <raiceviclav2107@gmail.com>
2025-03-25 19:09:13 +0100

    One random letter generated

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function check(){
+    let loewCheck = lowercase.checked
     let upCheck = uppercase.checked
     let numCheck = numbers.checked
     let symCheck = symbols.checked
 
-    let checkList = [upCheck, numCheck, symCheck];
+    let checkList = [loewCheck, upCheck, numCheck, symCheck];
 
     let result = checkList.filter((res)=> res == true)
 
     const boxes = document.querySelectorAll(".strength-box");
-    console.log(boxes);
 
     if(result.length == 0){
 
@@ -40,10 +40,26 @@ function check(){
         boxes[1].style.borderColor = "gold";
 
     } if(result.length == 3){
+
+        boxes.forEach(box => {
+            box.style.backgroundColor = "white";
+            box.style.borderColor = "white";
+        });
+
+        boxes[0].style.backgroundColor = "orange";
+        boxes[0].style.borderColor = "orange";
+
+        boxes[1].style.backgroundColor = "orange";
+        boxes[1].style.borderColor = "orange";
+
+        boxes[2].style.backgroundColor = "orange";
+        boxes[2].style.borderColor = "orange";
+
+    } if(result.length === 4) {
         boxes.forEach(box => {
             box.style.backgroundColor = "green";
             box.style.borderColor = "green";
-        })
+        });
     }
 }
 
@@ -56,9 +72,12 @@ range.addEventListener('input', e => {
     charLen.textContent = range.value;
 })
 
+const lowercase = document.getElementById("lowercase");
 const uppercase = document.getElementById("uppercase");
 const numbers = document.getElementById("numbers");
 const symbols = document.getElementById("symbols");
+
+lowercase.addEventListener("change", () => check());
 
 uppercase.addEventListener("change", () => check());
 
@@ -66,3 +85,23 @@ numbers.addEventListener("change", () => check());
 
 symbols.addEventListener("change", () => check());
 
+
+function generatePassword(){
+
+    let uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    let numbersChars = "0123456789";
+    let symbolsChars = "~!@#$%^&*_+-?><";
+
+    return(uppercaseChars);
+    
+}
+
+
+function generateChar(generator){
+    let character =  generator.charAt(Math.floor(Math.random() * 26));
+
+    return character
+}
+
+console.log(generateChar(generatePassword()))
